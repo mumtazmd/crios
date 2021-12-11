d9118a9eb26bcb01e9b1b5c207994667d7d1dc21
Mohammed <mumtazmd78@gmail.com>
2021-12-11 14:21:21 +0530

    Add files via upload

---
+++ src/app/mainpage/subscribefoo/subscribefoo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribefoo',
+  templateUrl: './subscribefoo.component.html',
+  styleUrls: ['./subscribefoo.component.scss']
+})
+export class SubscribefooComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
