ed874f5d1269b63360841ebb417057ff42fbfe3b
Mohammed <mumtazmd78@gmail.com>
2021-12-11 14:03:52 +0530

    Add files via upload

---
+++ src/app/mainpage/orgtestimonials/orgtestimonials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orgtestimonials',
+  templateUrl: './orgtestimonials.component.html',
+  styleUrls: ['./orgtestimonials.component.scss']
+})
+export class OrgtestimonialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
